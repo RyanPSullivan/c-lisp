9782d22dd1e357ca3b3e59267a585e9e6c640268
Ryan Sullivan <ryan.philip.sullivan@gmail.com>
2016-03-22 20:47:17 +0000

    start parsing a lisp like language

--- src/main.c
+++ src/main.c
@@ -3,21 +3,47 @@
 
 #include <histedit.h>
 
+#include "mpc/mpc.h"
+
 int main(int argc, char** argv) {
-   
+
+  /* Create Some Parsers */
+  auto number   = mpc_new("number");
+  auto operator = mpc_new("operator");
+  auto expr     = mpc_new("expr");
+  auto lispy    = mpc_new("lispy");
+
+  mpca_lang(MPCA_LANG_DEFAULT,
+	  "number    : /-?[0-9]+/;                             \
+           operator  : '+' | '-' | '*' | '/';                  \
+           expr      : <number> | '(' <operator> <expr>+ ')';  \
+           lispy     : /^/ <operator> <expr>+ /$/;             \
+           ", number, operator, expr, lispy);
+
   puts("Lispy Version 0.0.0.0.1");
   puts("Press Ctrl+c to Exit\n");
    
   while (1) {
-    
     /* Now in either case readline will be correctly defined */
-    char* input = readline("lispy> ");
+    char* input = readline("lisp > ");
     add_history(input);
 
+    mpc_result_t r;
+    if( mpc_parse("<stdin>", input, lispy, &r) )
+    {
+	    mpc_ast_print(r.output);
+	    mpc_ast_delete(r.output);
+    }
+    else
+    {
+	    mpc_err_print(r.error);
+	    mpc_err_delete(r.error);
+    }
     printf("No you're a %s\n", input);
     free(input);
     
   }
-  
+
+  mpc_cleanup(4, number, operator, expr, lispy);
   return 0;
 }
